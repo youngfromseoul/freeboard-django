43b667b7aebc11869e28ec59e021223d0be44999
youngfromseoul <youngfromseoul@gmail.com>
2020-09-06 22:32:23 +0900

    python-study

---
+++ venvs/mysite/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/freey/Library/Mobile Documents/com~apple~CloudDocs/work/python/venvs/mysite/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
